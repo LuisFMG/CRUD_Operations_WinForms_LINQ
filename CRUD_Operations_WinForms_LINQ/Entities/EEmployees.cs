e80253b07d6aa6c638f9d5f8cf48dda4b291c318
Luis F. Molina <LuisMG.31@hotmail.com>
2016-03-15 23:16:11 -0700

    Escritura de propiedades en 'EEmployees', finalizada

---
+++ CRUD_Operations_WinForms_LINQ/Entities/EEmployees.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CRUD_Operations.Entities
+{
+    public class EEmployees
+    {
+        public int IdEmployee { get; set; }
+
+        public DateTime DateOfHire { get; set; }
+
+        public byte[] PhotoEmployee { get; set; }
+
+        public string EmployeeName { get; set; }
+
+        public string EmployeeFamilyName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public int Age { get; set; }
+
+        public string Email { get; set; }
+
+        public string Username { get; set; }
+
+        public string UserPassword { get; set; }
+    }
+}
