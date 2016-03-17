6c72d05e939f26f164c2e53da0088a398db49a19
Luis F. Molina <LuisMG.31@hotmail.com>
2016-03-16 17:56:31 -0700

    Formulario 'PSearchEmployees', finalizado

---
+++ CRUD_Operations_WinForms_LINQ/PresentationLayer/PSearchEmployees.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace CRUD_Operations.Presentation
+{
+    partial class PSearchEmployees
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle16 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle17 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle18 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle19 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle20 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.DgvEmployees = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BtnExitSearchEmployees = new System.Windows.Forms.Button();
+            this.TxtIdEmployee = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvEmployees)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DgvEmployees
+            // 
+            this.DgvEmployees.AllowUserToAddRows = false;
+            dataGridViewCellStyle16.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.DgvEmployees.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle16;
+            dataGridViewCellStyle17.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle17.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle17.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle17.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle17.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle17.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle17.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DgvEmployees.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle17;
+            this.DgvEmployees.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvEmployees.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column8,
+            this.Column9,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7});
+            dataGridViewCellStyle18.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle18.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle18.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle18.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle18.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle18.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle18.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.DgvEmployees.DefaultCellStyle = dataGridViewCellStyle18;
+            this.DgvEmployees.Location = new System.Drawing.Point(12, 83);
+            this.DgvEmployees.Name = "DgvEmployees";
+            this.DgvEmployees.ReadOnly = true;
+            dataGridViewCellStyle19.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle19.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle19.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle19.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle19.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle19.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle19.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DgvEmployees.RowHeadersDefaultCellStyle = dataGridViewCellStyle19;
+            dataGridViewCellStyle20.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.DgvEmployees.RowsDefaultCellStyle = dataGridViewCellStyle20;
+            this.DgvEmployees.Size = new System.Drawing.Size(1092, 457);
+            this.DgvEmployees.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Id Empleado";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 92;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Usuario";
+            this.Column8.Name = "Column8";
+            this.Column8.ReadOnly = true;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "Contraseña";
+            this.Column9.Name = "Column9";
+            this.Column9.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Fecha de contratación";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 140;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Nombre";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 140;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Apellidos";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 140;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Fecha de nacimiento";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.Width = 132;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Edad";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            this.Column6.Width = 44;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Correo electrónico";
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            this.Column7.Width = 160;
+            // 
+            // BtnExitSearchEmployees
+            // 
+            this.BtnExitSearchEmployees.Location = new System.Drawing.Point(521, 565);
+            this.BtnExitSearchEmployees.Name = "BtnExitSearchEmployees";
+            this.BtnExitSearchEmployees.Size = new System.Drawing.Size(75, 23);
+            this.BtnExitSearchEmployees.TabIndex = 2;
+            this.BtnExitSearchEmployees.Text = "Salir";
+            this.BtnExitSearchEmployees.UseVisualStyleBackColor = true;
+            this.BtnExitSearchEmployees.Click += new System.EventHandler(this.BtnExitSearchEmployees_Click);
+            // 
+            // TxtIdEmployee
+            // 
+            this.TxtIdEmployee.Location = new System.Drawing.Point(119, 32);
+            this.TxtIdEmployee.MaxLength = 4;
+            this.TxtIdEmployee.Name = "TxtIdEmployee";
+            this.TxtIdEmployee.Size = new System.Drawing.Size(36, 20);
+            this.TxtIdEmployee.TabIndex = 0;
+            this.TxtIdEmployee.Text = "0";
+            this.TxtIdEmployee.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.TxtIdEmployee.TextChanged += new System.EventHandler(this.TxtIdEmployee_TextChanged_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ingresar Id:";
+            // 
+            // PSearchEmployees
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1116, 610);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtIdEmployee);
+            this.Controls.Add(this.BtnExitSearchEmployees);
+            this.Controls.Add(this.DgvEmployees);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "PSearchEmployees";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lista De Empleados";
+            this.Load += new System.EventHandler(this.PSearchEmployees_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DgvEmployees)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DgvEmployees;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.TextBox TxtIdEmployee;
+        private System.Windows.Forms.Button BtnExitSearchEmployees;      
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ CRUD_Operations_WinForms_LINQ/PresentationLayer/PSearchEmployees.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows.Forms;
+using CRUD_Operations.Business;
+
+namespace CRUD_Operations.Presentation
+{
+    public partial class PSearchEmployees : Form
+    {
+        public PSearchEmployees()
+        {
+            InitializeComponent();
+        }
+
+        private void PSearchEmployees_Load(object sender, EventArgs e)
+        {
+            BEmployees BEmployees = new BEmployees();
+
+            if (BEmployees.SearchEmployee(int.Parse(TxtIdEmployee.Text), DgvEmployees) == true)
+            {
+                DgvEmployees.Columns[0].DataPropertyName = "Id_User";
+                DgvEmployees.Columns[1].DataPropertyName = "Username";
+                DgvEmployees.Columns[2].DataPropertyName = "User_Password";
+                DgvEmployees.Columns[3].DataPropertyName = "Date_Of_Hire";
+                DgvEmployees.Columns[4].DataPropertyName = "Employee_Name";
+                DgvEmployees.Columns[5].DataPropertyName = "Employee_Family_Name";
+                DgvEmployees.Columns[6].DataPropertyName = "Date_Of_Birth";
+                DgvEmployees.Columns[7].DataPropertyName = "Age";
+                DgvEmployees.Columns[8].DataPropertyName = "Email";
+            }
+            else
+            {
+                MessageBox.Show("Carga de empleados fallida", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void BtnExitSearchEmployees_Click(object sender, EventArgs e)
+        {
+            PMainMenu OpenMainMenu = new PMainMenu();
+
+            Close();
+            OpenMainMenu.Show();
+        }
+
+        private void TxtIdEmployee_TextChanged_1(object sender, EventArgs e)
+        {
+            int IdEmployee = 0;
+            BEmployees BEmployees = new BEmployees();
+
+            if (TxtIdEmployee.TextLength == 4)
+            {
+                BEmployees.SearchEmployee(int.Parse(TxtIdEmployee.Text), DgvEmployees);
+            }
+            else
+            {
+                BEmployees.SearchEmployee(IdEmployee, DgvEmployees);
+            }
+        }
+    }
+}
